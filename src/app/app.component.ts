e9bee3986453916936df3dd591b70ca6776ae156
oiricaud <oiricaud@miners.utep.edu>
2019-07-20 12:19:24 -0600

    created alpha beta gamma values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,14 @@ import {Component, OnInit} from '@angular/core';
    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-
+   alpha: any = 0;
+   beta: any = 0;
+   gamma: any = 0;
    ngOnInit() {
       window.addEventListener('deviceorientation', function(event) {
+         this.alpha = event.alpha;
+         this.beta = event.beta;
+         this.gamma = event.gamma;
          console.log(event.alpha + ' : ' + event.beta + ' : ' + event.gamma);
       });
    }
