2a0e22887ac7f1ebe43a60b353896d12d221fb60
oiricaud <oiricaud@miners.utep.edu>
2019-07-20 10:28:47 -0600

    added magnetmoter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,16 @@ import {Component, OnInit} from '@angular/core';
 export class AppComponent implements OnInit {
 
    ngOnInit() {
-      let magSensor: any;
+      let magSensor = new Magnetometer({frequency: 60});
 
       magSensor.addEventListener('reading', e => {
          console.log("Magnetic field along the X-axis " + magSensor.x);
          console.log("Magnetic field along the Y-axis " + magSensor.y);
          console.log("Magnetic field along the Z-axis " + magSensor.z);
-      });
+      })
       magSensor.addEventListener('error', event => {
          console.log(event.error.name, event.error.message);
-      });
+      })
       magSensor.start();
    }
 
