70dbeac8a9e84ec23d1e2d6a207207ee7f3f1c13
oiricaud <oiricaud@miners.utep.edu>
2019-07-20 12:12:22 -0600

    udpated the magnometer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,9 @@ import {Component, OnInit} from '@angular/core';
 export class AppComponent implements OnInit {
 
    ngOnInit() {
-      let magSensor = new Magnetometer({frequency: 60});
-
-      magSensor.addEventListener('reading', e => {
-         console.log("Magnetic field along the X-axis " + magSensor.x);
-         console.log("Magnetic field along the Y-axis " + magSensor.y);
-         console.log("Magnetic field along the Z-axis " + magSensor.z);
-      })
-      magSensor.addEventListener('error', event => {
-         console.log(event.error.name, event.error.message);
-      })
-      magSensor.start();
+      window.addEventListener('deviceorientation', function(event) {
+         console.log(event.alpha + ' : ' + event.beta + ' : ' + event.gamma);
+      });
    }
 
 }
