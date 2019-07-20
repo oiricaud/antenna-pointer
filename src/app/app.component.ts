b7b8e45272df66ac16565e8ffddff239afbbe727
oiricaud <oiricaud@miners.utep.edu>
2019-07-20 10:27:03 -0600

    updated app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'antenna-pointer';
+export class AppComponent implements OnInit {
+
+   ngOnInit() {
+      let magSensor: any;
+
+      magSensor.addEventListener('reading', e => {
+         console.log("Magnetic field along the X-axis " + magSensor.x);
+         console.log("Magnetic field along the Y-axis " + magSensor.y);
+         console.log("Magnetic field along the Z-axis " + magSensor.z);
+      });
+      magSensor.addEventListener('error', event => {
+         console.log(event.error.name, event.error.message);
+      });
+      magSensor.start();
+   }
+
 }
