7c2afcf9e4272e2431e1fa68b1bbc065e5abb1d7
oiricaud <oiricaud@miners.utep.edu>
2019-07-20 23:56:38 -0600

    creating north and south

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,14 +12,27 @@ export class AppComponent implements OnInit {
    ngOnInit() {
       window.addEventListener('deviceorientation',
          function(event) {
-            window.alert('event.gamma ' + event.gamma);
-            var inputElement = document.getElementById('a');
-            var inputElement2 = document.getElementById('b');
-            var inputElement3 = document.getElementById('c');
+            var inputElement = document.getElementById('a'); // z axis [0, 360]
+            var inputElement2 = document.getElementById('b'); //x axis [-180, 180]
+            var inputElement3 = document.getElementById('c'); // y axis [-90, 90]
+            var inputElement4 = document.getElementById('heading'); // y axis [-90, 90]
+
+            var heading  = 360 - event.alpha; //heading [0, 360)
+
             inputElement.textContent = JSON.stringify(event.alpha);
             inputElement2.textContent = JSON.stringify(event.beta);
             inputElement3.textContent = JSON.stringify(event.gamma);
-      }, false);
+            inputElement4.textContent = JSON.stringify(heading);
+
+            if (heading > 359 || heading < 1) { //Allow +- 1 degree
+               document.body.style.backgroundColor = "green";
+               document.getElementById("heading").innerHTML = "N"; // North
+            }
+            else if (heading > 179 && heading < 181){ //Allow +- 1 degree
+               document.body.style.backgroundColor = "green";
+               document.getElementById("heading").innerHTML = "S"; // South
+            }
+         }, false);
    }
 }
    
