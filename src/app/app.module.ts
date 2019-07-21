6dcf72ddfed49810f3fe4fb77f620e0d1d4cb1cf
oiricaud <oiricaud@miners.utep.edu>
2019-07-21 16:59:31 -0600

    added the second slide with satellites and loading icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
+import { SatelliteComponent } from './satellite/satellite.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoadingSpinnerComponent,
+    SatelliteComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/satellite/satellite.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-satellite',
+  templateUrl: './satellite.component.html',
+  styleUrls: ['./satellite.component.scss']
+})
+export class SatelliteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
